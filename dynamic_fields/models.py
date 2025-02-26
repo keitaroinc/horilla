ba02f720a84d9fea52f2043d22e6a779a640a5c7
Horilla <support@horilla.com>
2025-02-26 10:56:53 +0530

    [UPDT] Pylint updates

--- dynamic_fields/models.py
+++ dynamic_fields/models.py
@@ -1,17 +1,16 @@
-import logging, re
+import logging
+import re
 from uuid import uuid4
 
 from django import forms
-from django.utils.translation import gettext_lazy as _
+from django.core.management import call_command
 from django.db import models
 from django.utils import timezone
-from django.core.management import call_command
+from django.utils.translation import gettext_lazy as _
+
 from dynamic_fields.df_not_allowed_models import DF_NOT_ALLOWED_MODELS
 from horilla.horilla_middlewares import _thread_locals
-
-
 from horilla_automations.methods.methods import get_model_class
-
 
 logger = logging.getLogger(__name__)
 
