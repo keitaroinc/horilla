e0772007910079e80a41a43cb0207f95f141e2bf
jaikishanwadhawa <jaikishanwadhawa@ymail.com>
2025-02-26 10:53:49 +0530

    add support for gcp bucket for media (#529)
    Co-authored-by: Jai <jai@voicegain.ai>
    Co-authored-by: Horilla <131998600+horilla-opensource@users.noreply.github.com>

--- horilla/horilla_settings.py
+++ horilla/horilla_settings.py
@@ -104,3 +104,29 @@ NO_PERMISSION_MODALS = [
     "resume",
     "recruitmentmailtemplate",
 ]
+
+if settings.env("AWS_ACCESS_KEY_ID", default=None):
+    AWS_ACCESS_KEY_ID = settings.env("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = settings.env("AWS_SECRET_ACCESS_KEY")
+    AWS_STORAGE_BUCKET_NAME = settings.env("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_REGION_NAME = settings.env("AWS_S3_REGION_NAME")
+    DEFAULT_FILE_STORAGE = settings.env("DEFAULT_FILE_STORAGE")
+    AWS_S3_ADDRESSING_STYLE = settings.env("AWS_S3_ADDRESSING_STYLE")
+
+    settings.AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
+    settings.AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
+    settings.AWS_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME
+    settings.AWS_S3_REGION_NAME = AWS_S3_REGION_NAME
+    settings.DEFAULT_FILE_STORAGE = DEFAULT_FILE_STORAGE
+    settings.AWS_S3_ADDRESSING_STYLE = AWS_S3_ADDRESSING_STYLE
+
+if settings.env("GOOGLE_APPLICATION_CREDENTIALS", default=None):
+    GS_BUCKET_NAME = settings.env("GS_BUCKET_NAME")
+    DEFAULT_FILE_STORAGE = settings.env("DEFAULT_FILE_STORAGE")
+
+    settings.GS_BUCKET_NAME = GS_BUCKET_NAME
+    settings.DEFAULT_FILE_STORAGE = DEFAULT_FILE_STORAGE
+
+if (settings.env("GOOGLE_APPLICATION_CREDENTIALS", default=None) or settings.env("AWS_ACCESS_KEY_ID", default=None)) and "storages" in INSTALLED_APPS:
+    settings.MEDIA_URL = f"{settings.env('MEDIA_URL')}/{settings.env('NAMESPACE')}/"
+    settings.MEDIA_ROOT = f"{settings.env('MEDIA_ROOT')}/{settings.env('NAMESPACE')}/"
