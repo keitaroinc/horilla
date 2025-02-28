b5ad22273cd410c2992fbd9ee433564ee217474f
Horilla <support@horilla.com>
2025-02-28 11:11:57 +0530

    [UPDT] RECRUITMENT: Updated send mail html part by replace style and functionalities of Write & Preview buttons

--- recruitment/models.py
+++ recruitment/models.py
@@ -510,6 +510,9 @@ class Candidate(HorillaModel):
         """ """
         return self.get_email()
 
+    def phone(self):
+        return self.mobile
+
     def tracking(self):
         """
         This method is used to return the tracked history of the instance
