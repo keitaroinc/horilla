4c9d8ccaaa76a772b84304aff6176ad49e963a6d
Marko Bocevski <marko.bocevski@keitaro.com>
2025-03-03 20:14:29 +0100

    Add CORS Allowed Origins setting

--- horilla/settings.py
+++ horilla/settings.py
@@ -28,6 +28,7 @@ DEBUG=eval(os.getenv("DEBUG", False))
 SECRET_KEY=os.getenv("SECRET_KEY", "django-insecure-j8op9)1q8$1&0^s&p*_0%d#pr@w9qj@1o=3#@d=a(^@9@zd@%j")
 ALLOWED_HOSTS=(os.getenv("ALLOWED_HOSTS", "*")).split()
 CSRF_TRUSTED_ORIGINS=(os.getenv("CSRF_TRUSTED_ORIGINS", "http://localhost:8000")).split()
+CORS_ALLOWED_ORIGINS=(os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:8000")).split()
 TIME_ZONE=os.getenv("TIME_ZONE", "Europe/Stockholm")
 
 # Route settings enables routes such as /login /admin ...
