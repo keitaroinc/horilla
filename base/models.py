0a2e4ea98c5483b04644ae9a91055130ce2f58a1
Horilla <support@horilla.com>
2025-03-08 13:27:45 +0530

    [UPDT] BASE: Updated base app forms and models

--- base/models.py
+++ base/models.py
@@ -147,7 +147,9 @@ class JobPosition(HorillaModel):
     JobPosition model
     """
 
-    job_position = models.CharField(max_length=50, blank=False, null=False)
+    job_position = models.CharField(
+        max_length=50, blank=False, null=False, verbose_name=_("Job Position")
+    )
     department_id = models.ForeignKey(
         Department,
         on_delete=models.PROTECT,
