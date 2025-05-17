89f8e060764231e2b3c80dc7c602513becd28796
Horilla <support@horilla.com>
2025-05-17 14:08:27 +0530

    [UPDT] BASE: Updated base app by modify some translator and horilla_crumbs files

--- onboarding/models.py
+++ onboarding/models.py
@@ -52,8 +52,8 @@ class OnboardingStage(HorillaModel):
         Meta class for additional options
         """
 
-        verbose_name = _("Stage")
-        verbose_name_plural = _("Stages")
+        verbose_name = _("Onboarding Stage")
+        verbose_name_plural = _("Onboarding Stages")
         ordering = ["sequence"]
 
 
